a0f3e387410ad06933faa3c2b52e161dd7ea0532
Betanzos <jb.rociana@gmail.com>
2014-06-23 21:11:38 +0200

    Comenzado crear destinos

--- ErasmusAndroid/src/com/mant/coordinador/CrearDestinoActivity.java
+++ ErasmusAndroid/src/com/mant/coordinador/CrearDestinoActivity.java
@@ -3,21 +3,31 @@ package com.mant.coordinador;
 import com.example.erasmusandroid.R;
 import com.example.erasmusandroid.R.layout;
 import com.example.erasmusandroid.R.menu;
+import com.mant.TareasAsincronas.SessionManager;
+import com.mant.TareasAsincronas.aTaskConsultarDestinos;
+import com.mant.TareasAsincronas.aTaskCrearDestinos;
+import com.mant.TareasAsincronas.aTaskLogin;
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
+import android.widget.EditText;
 
 // En esta clase habría que crear una función que guardará los datos
 //que introduce el coordinador en la base de datos
 
 public class CrearDestinoActivity extends Activity {
+	
+	// Session Manager Class
+    public SessionManager session;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_crear_destino);
+		
+		session = new SessionManager(getApplicationContext());
 	}
 
 	@Override
@@ -34,6 +44,17 @@ public class CrearDestinoActivity extends Activity {
 	}
 	
 	public void clickFinalizar(View v){
+		String nombre = ((EditText) findViewById(R.id.acd_nombre)).getText().toString();
+		int pais = Integer.parseInt(((EditText) findViewById(R.id.acd_pais)).getText().toString());
+		int idioma = Integer.parseInt(((EditText) findViewById(R.id.acd_pais)).getText().toString());
+		int disponible = Integer.parseInt(((EditText) findViewById(R.id.acd_pais)).getText().toString());
+		int plazas = Integer.parseInt(((EditText) findViewById(R.id.acd_pais)).getText().toString());
+		int nivel = Integer.parseInt(((EditText) findViewById(R.id.acd_pais)).getText().toString());
+	
+		session.checkLogin(); 
+		
+		aTaskCrearDestinos atl = new aTaskCrearDestinos(this, session,nombre,pais,idioma,disponible,plazas,nivel);
+		atl.execute();
 		finish();
 		
 	}
